0e2eadb6f465327472b88e8470dee55876272a37
mateus <89811139+Kmeliansky@users.noreply.github.com>
2022-09-01 14:29:31 -0300

    Add files via upload

---
+++ senhor_cafe/js/autoriza.js
@@ -0,0 +1,26 @@
+function nao(){
+    alert("Infelizmente você não pode preencher a este formulário ")
+}
+
+function nome(){
+    alert("Informe o seu Nome completo ")
+}
+function data(){
+    alert("Informe a sua data de nascimento ")
+}
+
+function email(){
+    alert("Informe seu email atual ")
+}
+
+function numero(){
+    alert("Informe seu Numero de celular ")
+}
+
+function destinatario(){
+    alert("Informe quem ira receber o email ")
+}
+
+function outro(){
+    alert("Informe outro assunto especifico ")
+}
