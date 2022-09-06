ee1f0d6a1010a7a06e49312897fae1d0b099962c
mateus <89811139+Kmeliansky@users.noreply.github.com>
2022-09-06 15:08:41 -0300

    Add files via upload

--- senhor_cafe/js/autoriza.js
+++ senhor_cafe/js/autoriza.js
@@ -1,26 +1,26 @@
 function nao(){
-    alert("Infelizmente você não pode preencher a este formulário ")
+    alert("Infelizmente você não pode preencher a este Formulário ")
 }
 
 function nome(){
     alert("Informe o seu Nome completo ")
 }
 function data(){
-    alert("Informe a sua data de nascimento ")
+    alert("Informe a sua Data de Nascimento ")
 }
 
 function email(){
-    alert("Informe seu email atual ")
+    alert("Informe seu E-mail atual ")
 }
 
 function numero(){
-    alert("Informe seu Numero de celular ")
+    alert("Informe seu Número de Celular ")
 }
 
 function destinatario(){
-    alert("Informe quem ira receber o email ")
+    alert("Informe quem irá receber o E-mail ")
 }
 
 function outro(){
-    alert("Informe outro assunto especifico ")
+    alert("Informe outro Assunto Específico ")
 }
